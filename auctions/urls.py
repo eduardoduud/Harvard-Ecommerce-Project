bb99d52571528ecbaa2964941d0f85bfccbd0b87
Eduardo <eeadomingues@gmail.com>
2024-03-27 15:44:55 -0300

    add new listing, watchlist and categories basic logic

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("createlisting", views.createListing, name="createlisting"),
+    path("watchlist", views.watchList, name="watchlist"),
+    path("categories", views.categories, name="categories"),
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -3,6 +3,7 @@ from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import User
 
@@ -29,7 +30,6 @@ def login_view(request):
             })
     else:
         return render(request, "auctions/login.html")
-
 
 def logout_view(request):
     logout(request)
@@ -61,3 +61,14 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+@login_required(login_url='/login')
+def createListing(request):
+    return HttpResponseRedirect(reverse("index"))
+
+@login_required(login_url='/login')
+def watchList(request):
+    return HttpResponseRedirect(reverse("index"))
+
+def categories(request):
+    return HttpResponseRedirect(reverse("index"))
